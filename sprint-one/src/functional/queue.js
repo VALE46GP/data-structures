51c78eec9c9d16270082b1fb62dcb3e4a406d25e
Eric Callari <ericcallari1@gmail.com>
2018-09-19 12:34:06 -0700

    queue.js started

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    
+    first++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+      last--;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
