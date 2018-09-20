69cee0a528cb692a4d06e7300fb0fb52f11cc3a9
VALE46GP <ericdoescode@gmail.com>
2018-09-20 11:27:11 -0700

    first commit

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+          // var findTail = function(elem) {
+          //   // find tail
+          //   if (elem.next !== null) {
+          //     findTail(elem.next);
+          //   } else {
+          //   return elem;
+          //   }
+          // };
+          // list.tail = findTail(list.head);
+          // list.tail.next = value;
+    if (list.head !== null) {
+      list.tail = {value: value, next: null};
+    } else {
+      //list.head = {value: value, next: null};
+      list.head = {value: value, next: list.tail};
+    }
   };
 
   list.removeHead = function() {
+    //list.head = list.head.next;
   };
 
   list.contains = function(target) {
