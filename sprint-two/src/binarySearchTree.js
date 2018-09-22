d73083b4b1b52800e4381a7a635acf8dba8d7f32
VALE46GP <ericdoescode@gmail.com>
2018-09-21 21:27:27 -0700

    binary search tree started

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,28 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(binarySearchTree.prototype);
+  obj.value = value;
+  obj.left = {};
+  obj.right = {};
+  return obj;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value < this.value) {
+    // check for children.. if none, define.. if yes, recursion with invoke
+    this.left = BinarySearchTree(value);
+  } else if (value > this.value) {
+    this.right = BinarySearchTree(value);
+  }
+
+
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
 };
 
 
